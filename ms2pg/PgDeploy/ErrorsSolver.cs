54e6e171e3c4f64456e2aa15ad9211e251afe7c6
Ilia Pishchalnikov <ilya_chaik@mail.ru>
2024-03-21 20:27:36 +0300

    show error position and message in files

--- ms2pg/PgDeploy/ErrorsSolver.cs
+++ ms2pg/PgDeploy/ErrorsSolver.cs
@@ -12,35 +12,51 @@ namespace ms2pg.PgDeploy
         {
             var batch = batches[batchIndex];
             string fixedBatch = null;
+            int position = 1;
+            var beforeErrorBatchPart = string.Empty;
             switch(exception.Data["SqlState"])
             {
                 case "42883": // Operator does not exist
                     if ((exception.Data["MessageText"] as string)!.Contains("character varying + character varying"))
                     {
-                        var position = (int) exception.Data["Position"]!;
-                        string beforeErrorBatchPart = batch.Substring(0, position);
+                        position = (int) exception.Data["Position"]!;
+                        beforeErrorBatchPart = batch.Substring(0, position);
                         var plusSignIndex = Regex.Match(new string(beforeErrorBatchPart.Reverse().ToArray()), "/+").Index;
                         fixedBatch = beforeErrorBatchPart.Substring(0, position - plusSignIndex - 1) + "|| "
                                 + batch.Substring(position - plusSignIndex + 1);
                     }
                     break;
-                case """22P02""": // Invalid syntax for type
+                case "22P02": // 
                     if ((exception.Data["MessageText"] as string)!.Contains("integer: \"\""))
                     {
-                        var position = (int) exception.Data["Position"]!;
-                        string beforeErrorBatchPart = batch.Substring(0, position - 1);
+                        position = (int) exception.Data["Position"]!;
+                        beforeErrorBatchPart = batch.Substring(0, position - 1);
                         fixedBatch = beforeErrorBatchPart.Substring(0, position - 1) + "0"
                                 + batch.Substring(position + 1);
                     }
                     break;
             }
-            if (fixedBatch != null )
-            {
+
+            if (fixedBatch != null)
+            {    
                 batches[batchIndex] = fixedBatch;
                 File.WriteAllText(fileName, batches.Aggregate((x,y) => x + "\n{{GO}}\n" + y));
                 return true;
             }
-            return false;
+            else
+            {                    
+                if (exception.Data.Contains("Position"))
+                {
+                    position = (int) exception.Data["Position"]!;
+                }
+                beforeErrorBatchPart = batch.Substring(0, position > 0 ? position-1 : 0);
+                fixedBatch = beforeErrorBatchPart 
+                        + $"/*!ERROR HERE!: '{exception.Data["SqlState"] as string}:{exception.Data["MessageText"] as string}'*/"
+                        + batch.Substring(position);
+                batches[batchIndex] = fixedBatch;
+                File.WriteAllText(fileName, batches.Aggregate((x,y) => x + "\n{{GO}}\n" + y));
+                return false;
+            }
         }
     }
 }
